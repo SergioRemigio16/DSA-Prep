f03e1744126af1494f3e1471538218b312a07229
Sergio Remigio <sergioremigio16@gmail.com>
2026-03-13 12:56:29 -0600

    Finish concatenation-of-array problem

---
+++ arrays_and_hashing/09-concatenation-of-array/solution.go
@@ -0,0 +1,18 @@
+package main
+
+func getContcatenation(nums []int) []int {
+	n := len(nums)
+	ans := make([]int, 2*n)
+	for i := 0; i < n; i++ {
+		ans[i] = nums[i]
+		ans[i+n] = nums[i]
+	}
+	return ans
+}
+
+func main() {
+	input := []int{1, 4, 1, 2}
+	for _, number := range getContcatenation(input) {
+		println(number)
+	}
+}
